20bc2631b2e10c908e101bed1004cc57158b6027
v4rden <denys.variah@gmail.com>
2020-12-06 21:20:43 +0200

    Arguments

---
+++ Problems/Call me by my name/main.py
@@ -0,0 +1,1 @@
+add_book(title="The Asylum for Heroes")
---
+++ Problems/Double greeting/main.py
@@ -0,0 +1,6 @@
+# the following lines read names from the input, do not modify it, please
+name_1 = input()
+name_2 = input()
+
+greeting(name_1, name_2)
+greeting(name_2, name_1)
