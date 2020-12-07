59a840f034651369d3f8813655ec8dcb41bc5664
v4rden <denys.variah@gmail.com>
2020-12-07 20:55:17 +0200

    args

---
+++ Problems/Types/main.py
@@ -0,0 +1,7 @@
+# the variable "args" is already defined
+my_list = args[1:]  # your code here
+my_list = list(map(int, my_list))
+
+print(my_list)
+
+# further code of the script "process_four_numbers.py"
